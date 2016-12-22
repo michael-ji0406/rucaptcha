7b0687ce235fdc04ae0b08610e5f6f559d9c4e9f
michael-ji0406 <michael.ji@dji.com>
2016-12-22 22:05:07 +0800

    add logger for debug

--- lib/rucaptcha/controller_helpers.rb
+++ lib/rucaptcha/controller_helpers.rb
@@ -41,7 +41,7 @@ module RuCaptcha
     #
     def verify_rucaptcha?(resource = nil, opts = {})
       opts ||= {}
-
+      logger.info "======== key is #{opts[:key]}"
       store_info = RuCaptcha.cache.read(rucaptcha_sesion_key_key(opts[:key]))
       # make sure move used key
       RuCaptcha.cache.delete(rucaptcha_sesion_key_key(opts[:key])) unless opts[:keep_session]
